bfd63f2225d04affba12803d87d7294b5a7e8497
atit <atit@tuc.co.th>
2018-11-28 10:32:27 +0700

    Start App

---
+++ app/src/main/java/atit/tuc/co/th/usuicode/MainActivity.java
@@ -0,0 +1,13 @@
+package atit.tuc.co.th.usuicode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
